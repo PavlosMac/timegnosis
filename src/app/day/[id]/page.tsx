e769810e0e5c435bb99fbe503c1cee00b400b26c
Pavlos <pavloskoz@hotmail.com>
2025-04-11 16:11:13 +0200

    Fix day params.

--- src/app/day/[id]/page.tsx
+++ src/app/day/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { fetchGnosis } from "@/mongo/fetchGnosis";
 
 export default async function DayPage({ params }: { params: { id: string } }) {
-  const energy = parseInt(params.id, 10);
-  const data = await fetchGnosis(energy, "day");
+  const p =  await params;
+  const data = await fetchGnosis(parseInt(p.id), "day");
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold">Day {energy}</h1>
+      <h1 className="text-2xl font-bold">Day {p.id}</h1>
       <p className="mt-4">{data}</p>
     </div>
   );
